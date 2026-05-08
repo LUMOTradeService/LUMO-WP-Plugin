c97fccf8061f57688976ac11d69ee5ff410113b2
Lukáš Moravec <39536753+LUMOL0ki@users.noreply.github.com>
2026-05-08 10:46:20 +0200

    Created editor preview

--- src/blocks/category-with-all/edit.js
+++ src/blocks/category-with-all/edit.js
@@ -13,6 +13,10 @@ import { __ } from '@wordpress/i18n';
  */
 import { useBlockProps } from '@wordpress/block-editor';
 
+import { useSelect } from '@wordpress/data';
+
+import { Spinner } from '@wordpress/components';
+
 /**
  * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
  * Those files can contain any CSS code that gets applied to the editor.
@@ -30,9 +34,44 @@ import './editor.scss';
  * @return {Element} Element to render.
  */
 export default function Edit() {
+	const blockProps = useBlockProps();
+
+    const { categories, isResolving } = useSelect( ( select ) => {
+        const query = {
+            per_page: -1,
+            hide_empty: true,
+        };
+        
+        // Používáme novější selektor isResolving pro čistší kód
+        return {
+            categories: select( 'core' ).getEntityRecords( 'taxonomy', 'category', query ),
+            isResolving: select( 'core' ).isResolving( 'getEntityRecords', [ 'taxonomy', 'category', query ] ),
+        };
+    }, [] );
+
+	const allCategories = categories || [];
+
 	return (
-		<p { ...useBlockProps() }>
-			{ __( 'Category with all – hello from the editor!', 'category-with-all' ) }
-		</p>
+		<div { ...blockProps }>
+            { isResolving ? (
+                <Spinner />
+            ) : (
+                <ul className="wp-block-categories-list wp-block-categories">
+                    <li className="cat-item cat-item-all">
+                        <a href="#">{ __( 'All', 'lumo-wp-plugin' ) }</a>
+                    </li>
+                    
+                    { allCategories.map( ( category ) => (
+                        <li key={ category.id } className={ `cat-item cat-item-${ category.id }` }>
+                            <a href="#">{ category.name }</a>
+                        </li>
+                    ) ) }
+
+                    { ! isResolving && allCategories.length === 0 && (
+                        <li>{ __( 'No categories found.', 'lumo-wp-plugin' ) }</li>
+                    ) }
+                </ul>
+            ) }
+        </div>
 	);
 }
